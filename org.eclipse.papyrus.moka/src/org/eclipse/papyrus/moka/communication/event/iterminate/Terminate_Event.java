e2c1d15a84b63e1083391b8d0484ca3853411c5a
jeremie.tatibouet <jeremie.tatibouet@cea.fr>
2015-11-03 14:19:36 +0100

    Register oep.moka
    Change-Id: Ice9f1f9effe7777c1ee4e88c24c3a8f9a059ad56
    Signed-off-by: jeremie.tatibouet <jeremie.tatibouet@cea.fr>

---
+++ org.eclipse.papyrus.moka/src/org/eclipse/papyrus/moka/communication/event/iterminate/Terminate_Event.java
@@ -0,0 +1,91 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.moka.communication.event.iterminate;
+
+import org.eclipse.debug.core.DebugEvent;
+import org.eclipse.debug.core.model.IDebugElement;
+import org.eclipse.debug.core.model.IThread;
+import org.eclipse.papyrus.moka.communication.Marshaller;
+import org.eclipse.papyrus.moka.communication.event.EventMessage;
+
+/**
+ * An event message representing a Terminate event.
+ * This message is emitted by the AbstractExecution engine,
+ * to inform the debug target that it has properly terminated
+ * to execute.
+ *
+ */
+public class Terminate_Event extends EventMessage {
+
+	/**
+	 * The threads available at the execution engine when it emitted
+	 * this Terminate_Event message
+	 */
+	protected IThread[] threads;
+
+	/**
+	 * Construct a terminate event from the given source.
+	 * The source is usually the debug target. See Start_Event for the rationale.
+	 *
+	 * @param source
+	 *            The source for the terminate event
+	 */
+	public Terminate_Event(IDebugElement source, IThread[] threads) {
+		this.source = source;
+		this.eventKind = DebugEvent.TERMINATE;
+		this.threads = threads;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.papyrus.moka.communication.event.Event#marshal()
+	 */
+	@Override
+	public String marshal() {
+		return Marshaller.getInstance().terminate_event_marshal(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.papyrus.moka.communication.event.Event#getDebugEvent()
+	 */
+	@Override
+	public DebugEvent getDebugEvent() {
+		if (this.debugEvent == null) {
+			this.debugEvent = new DebugEvent(source, eventKind);
+		}
+		return this.debugEvent;
+	}
+
+	/**
+	 * Returns the source of this terminate event
+	 *
+	 * @return the source of this terminate event
+	 */
+	public IDebugElement getSource() {
+		return this.source;
+	}
+
+	/**
+	 * Returns the threads available at the execution engine when it emitted this Terminate_Event message
+	 *
+	 * @return The threads available at the execution engine when it emitted this Terminate_Event message
+	 */
+	public IThread[] getThreads() {
+		return this.threads;
+	}
+
+}
---
+++ org.eclipse.papyrus.moka/src/org/eclipse/papyrus/moka/engine/MokaExecutionEngineJob.java
@@ -0,0 +1,67 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  CEA LIST - Initial API and implementation
+ *
+ *****************************************************************************/
+package org.eclipse.papyrus.moka.engine;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.papyrus.moka.debug.MokaDebugTarget;
+
+/**
+ * A Job for hosting the execution of an engine
+ *
+ */
+public class MokaExecutionEngineJob extends Job {
+
+	/**
+	 * The engine performing the execution
+	 */
+	protected IExecutionEngine engine;
+
+	protected MokaDebugTarget debugTarget;
+
+	public MokaExecutionEngineJob(String name, IExecutionEngine engine) {
+		super(name);
+		this.engine = engine;
+	}
+
+	/**
+	 * Start the execution engine
+	 *
+	 * (non-Javadoc)
+	 *
+	 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		try {
+			this.engine.start();
+		} catch (IOException e) {
+			return new Status(IStatus.ERROR, "org.eclipse.papyrus.moka", e.getMessage());
+		}
+		return new Status(IStatus.OK, "org.eclipse.papyrus.moka", "Execution was successfull");
+	}
+
+	public void setDebugTarget(MokaDebugTarget debugTarget) {
+		this.debugTarget = debugTarget;
+	}
+
+	public MokaDebugTarget getDebugTarget() {
+		return this.debugTarget;
+	}
+
+}
