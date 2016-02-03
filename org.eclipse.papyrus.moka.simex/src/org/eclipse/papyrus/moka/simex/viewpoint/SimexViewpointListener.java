bfa0d41794a45810dc9f634a3f92205630331171
Sebastien Revol <sebastien.revol@cea.fr>
2016-02-03 19:19:25 +0100

    Migrated to new ElementTypes API (which doesn't work ;) ).
    Change-Id: I4eef68e025de329f7bd30287e2b95eb150694f9b
    Signed-off-by: Sebastien Revol <sebastien.revol@cea.fr>

--- org.eclipse.papyrus.moka.simex/src/org/eclipse/papyrus/moka/simex/viewpoint/SimexViewpointListener.java
+++ org.eclipse.papyrus.moka.simex/src/org/eclipse/papyrus/moka/simex/viewpoint/SimexViewpointListener.java
@@ -2,6 +2,7 @@ package org.eclipse.papyrus.moka.simex.viewpoint;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +10,7 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.papyrus.infra.core.services.ServiceException;
 import org.eclipse.papyrus.infra.elementtypesconfigurations.ElementTypeSetConfiguration;
 import org.eclipse.papyrus.infra.elementtypesconfigurations.registries.ElementTypeSetConfigurationRegistry;
 import org.eclipse.papyrus.infra.gmfdiag.dnd.policy.DropStrategyManager;
@@ -74,14 +76,25 @@ public class SimexViewpointListener implements IPolicyCheckerListener {
 	}
 
 
+	@SuppressWarnings("restriction")
 	protected void registerElementTypesConfiguration() {
 
-		ElementTypeSetConfiguration iptgConfig = getSimexElementTypesConfig(); 
+		ElementTypeSetConfiguration simexConfig = getSimexElementTypesConfig(); 
 		List<ElementTypeSetConfiguration> tmpCollection = new ArrayList<ElementTypeSetConfiguration>();
-		tmpCollection.add(iptgConfig);
-		ElementTypeSetConfigurationRegistry.getInstance().loadElementTypeSetConfigurations(tmpCollection);
-		ElementTypeSetConfigurationRegistry.getInstance().getElementTypeSetConfigurations().put(SIMEX_ELEMENT_TYPES_ID, iptgConfig);
+		tmpCollection.add(simexConfig);
+		String contextID;
+		try {
+			contextID = org.eclipse.papyrus.infra.services.edit.internal.context.TypeContext.getContext().getId();
+			ElementTypeSetConfigurationRegistry.getInstance().loadElementTypeSetConfigurations(contextID,tmpCollection);
+			Map<String, ElementTypeSetConfiguration> configMap = new HashMap<>();
+			configMap.put(SIMEX_ELEMENT_TYPES_ID, simexConfig);
+			ElementTypeSetConfigurationRegistry.getInstance().getElementTypeSetConfigurations().put(contextID, configMap);
 
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 
 	}
 
@@ -91,24 +104,44 @@ public class SimexViewpointListener implements IPolicyCheckerListener {
 		if (loadedConfig != null){
 			return loadedConfig ;
 		}else {
-			Map<String, ElementTypeSetConfiguration> elementTypesConfigs = ElementTypeSetConfigurationRegistry.getInstance().getElementTypeSetConfigurations();
-			ResourceSet  elementTypesResSet;
+			Map<String, Map<String, ElementTypeSetConfiguration>> elementTypesConfigs = ElementTypeSetConfigurationRegistry.getInstance().getElementTypeSetConfigurations();
+		
+			ResourceSet elementTypesResSet=null;
 			if (! elementTypesConfigs.isEmpty()){
-				ElementTypeSetConfiguration firstConfig = elementTypesConfigs.values().iterator().next();
-				elementTypesResSet = firstConfig.eResource().getResourceSet();
+				 
+				@SuppressWarnings("restriction")
+				ElementTypeSetConfiguration firstConfig;
+				try {
+					firstConfig = elementTypesConfigs.get(org.eclipse.papyrus.infra.services.edit.internal.context.TypeContext.getContext().getId()).values().iterator().next();
+					elementTypesResSet = firstConfig.eResource().getResourceSet();
+				} catch (ServiceException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 
 			}else  {
 				elementTypesResSet = new ResourceSetImpl();
 			}
-			Resource res = elementTypesResSet.getResource(URI.createURI(SIMEX_ELEMENT_TYPES_DEF_PATH), true);
-			loadedConfig = (ElementTypeSetConfiguration) res.getContents().get(0);
+			if (elementTypesResSet != null){
+				Resource res = elementTypesResSet.getResource(URI.createURI(SIMEX_ELEMENT_TYPES_DEF_PATH), true);
+				loadedConfig = (ElementTypeSetConfiguration) res.getContents().get(0);
+			}
+			
 			return loadedConfig;
 		}
 
 	}
 
 	public void removeElementTypesConfiguration() {
-		ElementTypeSetConfigurationRegistry.getInstance().unload(SIMEX_ELEMENT_TYPES_ID);
+		try {
+			String contextID = org.eclipse.papyrus.infra.services.edit.internal.context.TypeContext.getContext().getId();
+			ElementTypeSetConfigurationRegistry.getInstance().unload(contextID, SIMEX_ELEMENT_TYPES_ID);
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
