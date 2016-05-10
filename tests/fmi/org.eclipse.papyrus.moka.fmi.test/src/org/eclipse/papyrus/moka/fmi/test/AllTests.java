edbf6ed0c9047eb9c6cad360a091a1e7cc8f6bfd
Sebastien Revol <sebastien.revol@cea.fr>
2016-05-10 14:56:31 +0200

    Added tests and fixed build issues
    Change-Id: I8462b0e8e73407ce25df8d8b9a6cadd65d727099
    Signed-off-by: Sebastien Revol <sebastien.revol@cea.fr>

---
+++ tests/fmi/org.eclipse.papyrus.moka.fmi.test/src/org/eclipse/papyrus/moka/fmi/test/AllTests.java
@@ -0,0 +1,11 @@
+package org.eclipse.papyrus.moka.fmi.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TestFMI.class})public class AllTests {
+
+	
+}
