031bf6aa4b3e94ce96b2432e1dc3835e8e908328
Arnaud Cuccuru <arnaud.cuccuru@cea.fr>
2016-06-24 16:51:04 +0200

    [FMI] Adding a "New example" wizard
    Change-Id: Iccacfc2cf3617e95cf3f1e525570eb871b04fb92
    Signed-off-by: Arnaud Cuccuru <arnaud.cuccuru@cea.fr>

---
+++ bundles/fmi/ui/org.eclipse.papyrus.moka.fmi.ui/src/org/eclipse/papyrus/moka/fmi/ui/wizards/FMUTutoWizard.java
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * Copyright (c) 2013 CEA LIST.
+ *
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Ansgar Radermacher  ansgar.radermacher@cea.fr
+ *
+ *****************************************************************************/
+
+package org.eclipse.papyrus.moka.fmi.ui.wizards;
+
+import org.eclipse.papyrus.uml.diagram.wizards.wizards.ModelCopyWizard;
+
+/**
+ * Copy wizard for the HelloWorld example
+ */
+public class FMUTutoWizard extends ModelCopyWizard {
+
+	public FMUTutoWizard() {
+		super("TutoFMU"); //$NON-NLS-1$
+	}
+}
